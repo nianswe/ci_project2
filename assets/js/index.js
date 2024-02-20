19087a3ec0d8c7b8d5d6375f93fb52cf54dbdac9
Niklas Andersson <nianswe@hotmail.com>
2024-02-20 20:16:58 +0000

    JS create function to get 1 + 17 random images from array, function for position

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,24 +3,73 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
-
+    
     for (let button of buttons) {
         button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") === "submit") {
-                getimages();
-            }
+            /*if (this.getAttribute("data-type") === "startbutton") {
+            getImages();
+            } */
+            if (this.getAttribute("data-type") === "getDouble") {
+            getDouble();
+            } else if (this.getAttribute("data-type") === "getPosition") {
+							getPosition();
+						}
+
         });
     }
-
 });
-function getimages() {
 
-let images = ["img1", "img2", "img3", "img4", "img5", "img6", "img7", "img8", "img9", "img10", "img11", "img12", "img13", "img14", "img15", "img16", "img17", "img18", "img19", "img20", "img21", "img22", "img23", "img24", "img25", "img26", "img27", "img28", "img29", "img30"]
+/* Function GetImages, Get 18 random images of 30 from array 'images' pushed to array 'pickedImgs' */
+function getImages() {
+  let images = ["img1", "img2", "img3", "img4", "img5", "img6", "img7", "img8", "img9", "img10", "img11", "img12", "img13", "img14", "img15", "img16", "img17", "img18", "img19", "img20", "img21", "img22", "img23", "img24", "img25", "img26", "img27", "img28", "img29", "img30"]
+  var pickedImgs = [];    
+  for (let i = 0; i < 18; i++) {
     let random = Math.floor(Math.random() * images.length);
-    console.log(random, images[random]);
-        
+    	pickedImgs.push(images[random]);
+      images.splice(random, 1);
+    }
+    /* console.log(pickedImgs); */      
+   return pickedImgs;      
+};
 
-}
+/* Function generate Double image, Get 1 random images of 18 from array 'pickedImgs' pushed to array 'First8Images' */
+function getDouble() {
+  var pickedImgs = getImages();
+  console.log(pickedImgs);  
+  var pickedDouble = [];
+    let random = Math.floor(Math.random() * pickedImgs.length);
+    pickedDouble.push(pickedImgs[random]);
+    pickedImgs.splice(random, 1);
+    console.log(pickedImgs); 
+    console.log(pickedDouble);   
+    return pickedImgs;
+    return pickedDouble;
+		
+};
+
+function getPosition() {
+	var position = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+		let currentIndex = position.length,  randomIndex;
+	
+		// While there remain elements to shuffle.
+		while (currentIndex > 0) {
+	
+			// Pick a remaining element.
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex--;
+	
+			// And swap it with the current element.
+			[position[currentIndex], position[randomIndex]] = [
+				position[randomIndex], position[currentIndex]];
+		}
+		console.log(position);
+		return position;
+	}
+	
+	
+
+
+
 
 
 
