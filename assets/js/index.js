0b7ce9cc79c922c789521ab80068ae84038806f6
Niklas Andersson <nianswe@hotmail.com>
2024-02-26 14:42:47 +0000

    Documented all functions in index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,99 +1,86 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
-
 document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
-    
+let buttons = document.getElementsByTagName("button");
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "runGame") {
-			// start10SecTimer()
+			// StartTimer()
 				runGame();
 			// Quit Game Button
 			} else if (this.getAttribute("data-type") === "quitGame") {
 				location.reload();
-			
-			// } else if (this.getAttribute("data-type") === "shufAllImages") {
-			// shufAllImages();
-			// } else if (this.getAttribute("data-type") === "double") {
-			// double();
-		}
-			
+			}	
 		});
 	};
 });
 
-// Correct chosen div/cell card1
+
+// Variable for Correct chosen div/cell card1 for validation if correct image is picked
 var correct1; 
 // Value of child of correct chosen div/cell card1
 var correctImg1;
 
-
-
+// Function shuffle all images in array
 function shufAllImages() {
-	// let allImages = ['<i id= "1" class = "fa-solid fa-music"></i>', '<i id= "2" class = "fa-solid fa-star"></i>', '<i id= "3" class = "fa-solid fa-heart"></i>', '<i id= "4" class = "fa-regular fa-face-smile"></i>', '<i id= "5" class = "fa-regular fa-bell"></i>', '<i id= "6" class = "fa-solid fa-bolt"></i>', '<i id= "7" class = "fa-solid fa-car"></i>', '<i id= "8" class = "fa-solid fa-umbrella"></i>', '<i id= "9" class = "fa-solid fa-headphones"></i>', '<i id= "10" class = "fa-solid fa-plane"></i>', '<i id= "11" class = "fa-solid fa-key"></i>', '<i id= "12" class = "fa-solid fa-bicycle"></i>', '<i id= "13" class = "fa-solid fa-sun"></i>', '<i id= "14" class = "fa-solid fa-car-side"></i>', '<i id= "15" class = "fa-solid fa-robot"></i>', '<i id= "16" class = "fa-solid fa-trophy"></i>', '<i id= "17" class = "fa-solid fa-circle-check"></i>', '<i id= "18" class = "fa-solid fa-glasses"></i>', '<i id= "19" class = "fa-solid fa-yin-yang"></i>', '<i id= "20" class = "fa-solid fa-suitcase"></i>', '<i id= "21" class = "fa-solid fa-skull"></i>', '<i id= "22" class = "fa-solid fa-sailboat"></i>', '<i id= "23" class = "fa-solid fa-rainbow"></i>', '<i id= "24" class = "fa-solid fa-radiation"></i>', '<i id= "25" class = "fa-solid fa-pizza-slice"></i>', '<i id= "26" class = "fa-solid fa-peace"></i>', '<i id= "27" class = "fa-solid fa-hourglass-half"></i>', '<i id= "28" class = "fa-solid fa-guitar"></i>', '<i id= "29" class = "fa-solid fa-fish-fins"></i>', '<i id= "30" class = "fa-solid fa-clover"></i>'];
 	let allImages = ['<i id = "music" class = "fa-solid fa-music"></i>', '<i id = "star" class = "fa-solid fa-star"></i>', '<i id = "heart" class = "fa-solid fa-heart"></i>', '<i id = "face-smile" class = "fa-regular fa-face-smile"></i>', '<i id = "bell" class = "fa-regular fa-bell"></i>', '<i id = "bolt" class = "fa-solid fa-bolt"></i>', '<i id = "car" class = "fa-solid fa-car"></i>', '<i id = "umbrella" class = "fa-solid fa-umbrella"></i>', '<i id = "headphones" class = "fa-solid fa-headphones"></i>', '<i id = "plane" class = "fa-solid fa-plane"></i>', '<i id = "key" class = "fa-solid fa-key"></i>', '<i id = "bicycle" class = "fa-solid fa-bicycle"></i>', '<i id = "sun" class = "fa-solid fa-sun"></i>', '<i id = "car-side" class = "fa-solid fa-car-side"></i>', '<i id = "robot" class = "fa-solid fa-robot"></i>', '<i id = "trophy" class = "fa-solid fa-trophy"></i>', '<i id = "check" class = "fa-solid fa-circle-check"></i>', '<i id = "glasses" class = "fa-solid fa-glasses"></i>', '<i id = "yin-yang" class = "fa-solid fa-yin-yang"></i>', '<i id = "suitcase" class = "fa-solid fa-suitcase"></i>', '<i id = "skull" class = "fa-solid fa-skull"></i>', '<i id = "sailboat" class = "fa-solid fa-sailboat"></i>', '<i id = "rainbow" class = "fa-solid fa-rainbow"></i>', '<i id = "radiation" class = "fa-solid fa-radiation"></i>', '<i id = "pizza-slice" class = "fa-solid fa-pizza-slice"></i>', '<i id = "peace" class = "fa-solid fa-peace"></i>', '<i id = "hourglass" class = "fa-solid fa-hourglass-half"></i>', '<i id = "guitar" class = "fa-solid fa-guitar"></i>', '<i id = "fish" class = "fa-solid fa-fish-fins"></i>', '<i id = "clover" class = "fa-solid fa-clover"></i>'];
-	
-	let currentIndex = allImages.length,  randomIndex;
-  
-	// While there remain elements to shuffle.
+	let currentIndex = allImages.length, randomIndex;
+  	// While there remain elements to shuffle.
 	while (currentIndex > 0) {
-  
-	  // Pick a remaining element.
-	  randomIndex = Math.floor(Math.random() * currentIndex);
-	  currentIndex--;
-  
-	  // And swap it with the current element.
-	  [allImages[currentIndex], allImages[randomIndex]] = [
-		allImages[randomIndex], allImages[currentIndex]];
+  		// Pick a remaining element.
+	  	randomIndex = Math.floor(Math.random() * currentIndex);
+	  	currentIndex--;
+      // And swap it with the current element.
+	    [allImages[currentIndex], allImages[randomIndex]] = [allImages[randomIndex], allImages[currentIndex]];
 	}
-	console.log(allImages);
 	return allImages;
-  }
+}
 
+// Function shuffle all positions in array
 function shufOrgPositions() {
-	let orgPosition = ["pos1", "pos2", "pos3", "pos4", "pos5", "pos6", "pos7", "pos8", "pos9", "pos10", "pos11", "pos12", "pos13", "pos14", "pos15", "pos16", "pos17", "pos18"];
-	// let firstCardPos = ["pos1", "pos3", "pos5", "pos7", "pos9", "pos11", "pos13", "pos15", "pos17"];
-	let currentIndex = orgPosition.length,  randomIndex;
-  
-	// While there remain elements to shuffle.
+	// All positions
+  let orgPosition = ["pos1", "pos2", "pos3", "pos4", "pos5", "pos6", "pos7", "pos8", "pos9", "pos10", "pos11", "pos12", "pos13", "pos14", "pos15", "pos16", "pos17", "pos18"];
+	// Random all positions in array
+  let currentIndex = orgPosition.length, randomIndex;
+  // While there remain elements to shuffle.
 	while (currentIndex > 0) {
-  
-	  // Pick a remaining element.
+    // Pick a remaining element.
 	  randomIndex = Math.floor(Math.random() * currentIndex);
 	  currentIndex--;
-  
-	  // And swap it with the current element.
-	  [orgPosition[currentIndex], orgPosition[randomIndex]] = [
-		orgPosition[randomIndex], orgPosition[currentIndex]];
+    // And swap it with the current element.
+	  [orgPosition[currentIndex], orgPosition[randomIndex]] = [orgPosition[randomIndex], orgPosition[currentIndex]];
 	}
-	console.log(orgPosition);
 	return orgPosition;
 }
 
-// Function double to get double image position.
+// Functions to get double image position random one on each card.
+// Positions on upper card
 let firstCardPos = ["pos1", "pos3", "pos5", "pos7", "pos9", "pos11", "pos13", "pos15", "pos17"];
+// Positions on upper card
 let secondCardPos = ["pos2", "pos4", "pos6", "pos8", "pos10", "pos12", "pos14", "pos16", "pos18"];
+// Function to get double image position on upper card
 function doublePos1() {
 	var doublePos1 = firstCardPos[(Math.floor(Math.random() * firstCardPos.length))];
-	 console.log(doublePos1);
-	 return doublePos1
+	return doublePos1
 }
+// Function to get double image position on lower card
 function doublePos2() {
 	var doublePos2 = secondCardPos[(Math.floor(Math.random() * secondCardPos.length))];
-	 console.log(doublePos2);
-	 return doublePos2
+	return doublePos2
 }
 
+// Function to start game and get random images on the two cards
 function runGame() { 
-	stop10SecTimer();
-	start10SecTimer();
+	stopTimer();
+	startTimer();
 	hidetimeisUpInfo();
 	hideFinalScoreInfo();
 	hideRunGameButton();
 	
+  // All shuffled images 18 of total 30
 	let images = shufAllImages();
-	let position = shufOrgPositions();
+	// All shuffled positions on the two cards
+  let position = shufOrgPositions();
 	var pickedPos1 = [];
 	var pickedPos2 = [];
 	var pickedPos3 = [];
@@ -112,10 +99,10 @@ function runGame() {
 	var pickedPos16 = [];
 	var pickedPos17 = [];
 	var pickedPos18 = [];
-
+// Random positions for double image on the to cards
 	var doublePosition1 = doublePos1();
 	var doublePosition2 = doublePos2(); 
-
+// All shuffled images in own array
 	var pickedImg1 = [];
 	var pickedImg2 = [];
 	var pickedImg3 = [];
@@ -135,6 +122,7 @@ function runGame() {
 	var pickedImg17 = [];
 	var pickedImg18 = [];
 
+  // Each random position index in position array
 	pickedPos1 = position[0]; 
 	pickedPos2 = position[1]; 
 	pickedPos3 = position[2]; 
@@ -153,7 +141,9 @@ function runGame() {
 	pickedPos16 = position[15]; 
 	pickedPos17 = position[16]; 
 	pickedPos18  = position[17]; 
-	pickedImg1 = images[0]; 
+
+  // Each random image index in images array	
+  pickedImg1 = images[0]; 
 	pickedImg2 = images[1]; 
 	pickedImg3 = images[2]; 
 	pickedImg4 = images[3]; 
@@ -172,46 +162,8 @@ function runGame() {
 	pickedImg17 = images[16]; 
 	pickedImg18 = images[17];
 	doubleImage = images[18];  
-		
-	console.log("pickedPos1", pickedPos1);
-	console.log("pickedPos2", pickedPos2);
-	console.log("pickedPos3", pickedPos3);
-	console.log("pickedPos4", pickedPos4);
-	console.log("pickedPos5", pickedPos5);
-	console.log("pickedPos6", pickedPos6);
-	console.log("pickedPos7", pickedPos7);
-	console.log("pickedPos8", pickedPos8);
-	console.log("pickedPos9", pickedPos9);
-	console.log("pickedPos10", pickedPos10);
-	console.log("pickedPos11", pickedPos11);
-	console.log("pickedPos12", pickedPos12);
-	console.log("pickedPos13", pickedPos13);
-	console.log("pickedPos14", pickedPos14);
-	console.log("pickedPos15", pickedPos15);
-	console.log("pickedPos16", pickedPos16);
-	console.log("pickedPos17", pickedPos17);
-	console.log("pickedPos18", pickedPos18);
-	console.log("doubleImage ", doubleImage);
-	
-	console.log("pickedImg1", pickedImg1);
-	console.log("pickedImg2", pickedImg2);
-	console.log("pickedImg3", pickedImg3);
-	console.log("pickedImg4", pickedImg4);
-	console.log("pickedImg5", pickedImg5);
-	console.log("pickedImg6", pickedImg6);
-	console.log("pickedImg7", pickedImg7);
-	console.log("pickedImg8", pickedImg8);
-	console.log("pickedImg9", pickedImg9);
-	console.log("pickedImg10", pickedImg10);
-	console.log("pickedImg11", pickedImg11);
-	console.log("pickedImg12", pickedImg12);
-	console.log("pickedImg13", pickedImg13);
-	console.log("pickedImg14", pickedImg14);
-	console.log("pickedImg15", pickedImg15);
-	console.log("pickedImg16", pickedImg16);
-	console.log("pickedImg17", pickedImg17);
-	console.log("pickedImg18", pickedImg18);
 
+// Place each random image in each random position div in HTML
 	document.getElementById(pickedPos1).innerHTML = pickedImg1;
 	document.getElementById(pickedPos2).innerHTML = pickedImg2;
 	document.getElementById(pickedPos3).innerHTML = pickedImg3;
@@ -232,127 +184,156 @@ function runGame() {
 	document.getElementById(pickedPos18).innerHTML = pickedImg18;
 	document.getElementById(doublePosition1).innerHTML = doubleImage;
 	document.getElementById(doublePosition2).innerHTML = doubleImage;
-	console.log("doublePosition1",doublePosition1);
-	console.log("doublePosition2",doublePosition2);
-	console.log("doubleImage",doubleImage);
+	// Display correct Double Image in console at testing, out commented.
+  console.log("doubleImage",doubleImage);
 	
-	correct1 = document.getElementById(doublePosition1);
+  // Get correct position for double image on upper card to get Id of image to validate with choosen image 
+  correct1 = document.getElementById(doublePosition1);
 	correctImg1 = correct1.children[0];
 	onClickImg(correctImg1.id);
-	// console.log('correctImgId',correctImg1.id);
-	onClickImg();
+	// Display correct Double Image in console at testing, out commented.
+  // console.log('correctImgId',correctImg1.id);
+	
+  // Trigger On-Click function waiting for Click on image when image is choosen 
+  onClickImg();
 }
-
-function onClickImg(a) {
+ 
+// On-Click function waiting for Click on image when image is choosen on upper or bottom card .
+function onClickImg() {
 	picsClick = document.getElementsByTagName("i");
 		for( var x=0; x < picsClick.length; x++ ) {
-		picsClick[x].onclick = function(){
-			// alert("You picked the,"+this.id+", Correct image is the "+correctImg1.id);
-			// let correct = (doubleImage);
-			// let choice = ("Choice:,"+this.id+""+this.innerHTML);
-			let choice = (""+this.id+"");
-			// console.log('Correct:',correct);
-			console.log('choice', choice);
-			// console.log('correct1', correct1);
-			console.log('correctImg1', correctImg1.id);
+		  picsClick[x].onclick = function(){
+			  let choice = (""+this.id+"");
+			  // Display correct choosen Image in console, for testing, out commented.
+        console.log('choice', choice);
+			  // Display correct choosen Image in console, for testing, out commented.
+			  console.log('correctImg1', correctImg1.id);
 			
-			 
-			if (choice === correctImg1.id) {
-				stop10SecTimer();
-				showRunGameButton()
-				alert("     CORRECT ANSWER!     \nYou picked the:\n     "+this.id+"     \nCorrect image is the:\n     "+correctImg1.id);
-				console.log('CORRECT ANSWER!');
-				incrementScore();
-				runGame();
-			} else {
-				stop10SecTimer();
-				document.getElementById("finalScore2").innerText = "0";
-				showRunGameButton()
-				document.getElementById("score").innerText = "0";
-				alert("     WRONG ANSWER!     \nYou picked the: \n     "+this.id+"\n Correct image is the:\n     "+correctImg1.id);
-				console.log("WRONG ANSWER!\n You picked the,\n"+this.id+",\n Correct image is the\n "+correctImg1.id);
-				document.getElementById("score").innerText = finalScore;
-				showFinalScoreInfo();
-				resetScore();
-				return start10SecTimer;
-								
-			}
-		
-		}
-	}
+        // Validation to verify if correct image is choosen or not
+			    if (choice === correctImg1.id) {
+			      // Stop and reset Timer
+            stopTimer();
+			      // Make Run Game button visuable if timer is inactivated 
+            showRunGameButton()
+				    // Pop-up window Informing Correct Answer
+            alert("     CORRECT ANSWER!     \nYou picked the:\n     "+this.id+"     \nCorrect image is the:\n     "+correctImg1.id);
+				    // Trigger funktion to increase score by 1 in score section 
+				    incrementScore();
+				    // Trigger Run game function to get two new cards
+            runGame();
+			    } else {
+				    // Stop and reset Timer
+            stopTimer();
+				    // Reset final Score value on Time is up Window and Final score window to 0
+            document.getElementById("finalScore1").innerText = "0";
+            document.getElementById("finalScore2").innerText = "0";
+            console.log 
+				    // Make Run Game button visuable
+            showRunGameButton()
+				    // Reset Score value in Score section to 0
+            document.getElementById("score").innerText = "0";
+				    // Pop-up window Informing Incorrect Answer, innalScore2form choosen image and correct image
+            alert("     WRONG ANSWER!     \nYou picked the: \n     "+this.id+"\n Correct image is the:\n     "+correctImg1.id);
+				    //Add final score to final score window
+            document.getElementById("finalScore1").innerText = finalScore;
+				    // View Final Score Window with final score
+            showFinalScoreInfo();
+				    // Reset Score value in Score section to 0
+            resetScore();
+				    return startTimer;
+			    }
+      }  
+    }
 }
 
+// Increment Score Function
 var currentScore;
 var finalScore;
 function incrementScore() {  
+    // Get current score from score board
     currentScore = parseInt(document.getElementById("score").innerText);
+    //Set finalscore value is equal to currentScore value 
     finalScore = currentScore;
-	finalScore = ++finalScore;
-	document.getElementById("score").innerText = ++currentScore;
-	 
+	  // Increment final score value with one
+    finalScore = ++finalScore;
+	// Increas score value on score board with one
+  document.getElementById("score").innerText = ++currentScore;
+  // Set Final score value to Final score info Window.
 	document.getElementById("finalScore1").innerText = finalScore;
-	document.getElementById("finalScore2").innerText = finalScore;
-	console.log('currentScore',currentScore);
-	console.log('yfinalScore',finalScore);
-
+	// Set Final score value to Time Is Up Window.
+  document.getElementById("finalScore2").innerText = finalScore;
 }
 
+// Reset score on score board, Final score info Window, Time Is Up Window to 0
 function resetScore() {
 	currentScore = 0;
 	document.getElementById("score").innerText = "0";
+  // document.getElementById("finalScore1").innerText = "0";
+	// document.getElementById("finalScore2").innerText = "0";
 }
 
+// Show FinalScoreInfo Window, hidden by default.
 function showFinalScoreInfo() {
 	document.getElementById("finalScoreInfo").style.display = "block";
 }
-
+// Show RunGameButton, hidden during ongoing game.
 function showRunGameButton() {
 	document.getElementById("runGame").style.visibility = 'visible';
 }
-
+// Hide FinalScoreInfo Window when game starts
 function hideFinalScoreInfo() {
 	document.getElementById("finalScoreInfo").style.display = "none";
 }
-
+// Hide RunGameButton when game starts
 function hideRunGameButton() {
 	document.getElementById("runGame").style.visibility = 'hidden';
 }
-
+// Show timeisUpInfo window, hidden during ongoing game.
 function timeisUpInfo() {
 	document.getElementById("timeisUpInfo").style.display = "block";
 }
-
-
+// Hide timeisUpInfo window when game starts
 function hidetimeisUpInfo() {
 	document.getElementById("timeisUpInfo").style.display = "none";
 };
-
+// Count down timer Function
 var	coundownTimer;
-function start10SecTimer() {
-	let timeleft = 10;
+function startTimer() {
+	// Count down amount of seconds
+  let timeleft = 20;
 	coundownTimer = setInterval(function(){
-  		if(timeleft <= 0){
+  		// When it is out of time
+      if(timeleft <= 0){
     		clearInterval(coundownTimer);
-			stop10SecTimer();
-			showRunGameButton(); 
-			document.getElementById("seconds").innerHTML = timeleft;
-			timeisUpInfo();
-			resetScore();
-			console.log(currentScore);
+			stopTimer();
+			// Show RunGameButton
+      showRunGameButton(); 
+			// Show countdown seconds in span in Count-down div
+      document.getElementById("seconds").innerHTML = timeleft;
+			// Show timeisUpInfo window
+      timeisUpInfo();
+			// Reset score to 0
+      resetScore();
 			return;
+    
+    //Ongoing countdown
 		} else {
-    		hideFinalScoreInfo(); 
+    	// Hide FinalScoreInfo window
+      hideFinalScoreInfo();
+      // Hide hidetimeisUpInfo window 
 			hidetimeisUpInfo();
+      // Hide hideRunGameButton
 			hideRunGameButton();
-  			document.getElementById("progressBar").value = 11 - timeleft;
+  			//Update progress bar
+        document.getElementById("progressBar").value = 11 - timeleft;
+        //Update countdown timer
   			document.getElementById("seconds").innerHTML = timeleft +"";
 			timeleft -= 1;
-			console.log(timeleft);
 		}
 	}, 1000);
 }
 
-function stop10SecTimer() {
+function stopTimer() {
 	clearInterval(coundownTimer);
-	// return start10SecTimer;
+	
 }
